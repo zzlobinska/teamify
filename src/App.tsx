bd0619140dbdea43b4322a1b60fe327cb9bdf1a6
Zuza <z.j.zlobinska@gmail.com>
2023-02-02 16:15:27 +0100

    done user profile in user details page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {loader as userDetailLoader} from './Pages/DetailsPage/UserDetails/index'
+
 
 import './App.css';
-import RootLayout from './Components/Layout/RootLayout';
+
 import LandingPage from './Pages/LandingPage';
 
 import AboutUsPage from './Pages/AboutUsPage';
@@ -18,10 +20,13 @@ const router = createBrowserRouter([
 		element: <AboutUsPage />,
 	},
 	{
-		path: 'about-us/details',
+		path: 'about-us/:userId',
+		loader: userDetailLoader,
 		element: <DetailsPage />,
+		
 	},
 ]);
+
 
 function App() {
 	return <RouterProvider router={router} />;
--- src/Pages/DetailsPage/UserDetails/index.tsx
+++ src/Pages/DetailsPage/UserDetails/index.tsx
@@ -1,37 +1,76 @@
 import Button from '../../../Components/UI/Button';
 import styles from './UserDetails.module.scss';
-import { BsFileEarmarkCheckFill } from 'react-icons/bs';
+import {
+	BsFileEarmarkCheckFill,
+	BsFillArrowUpLeftSquareFill,
+} from 'react-icons/bs';
 import { BsFileEarmarkPersonFill } from 'react-icons/bs';
+import {
+	LoaderFunctionArgs,
+	useLoaderData,
+	useNavigate,
+} from 'react-router-dom';
+import axios from 'axios';
+
+type UserType = {
+	firstName: string;
+	lastName: string;
+	username: string;
+	height: number;
+	weight: number;
+	image: string;
+};
 
 const UserDetails = () => {
+	const userData: any = useLoaderData();
+	console.log('userData', userData);
+
+	const navigate = useNavigate();
+
 	return (
-		<div className={styles.content}>
-			<div className={styles.user}>
-				<img
-					className={styles.photo}
-					alt='a user'
-					src='https://scontent.fpoz3-1.fna.fbcdn.net/v/t31.18172-8/19055483_1188929134567023_7020530751396910719_o.jpg?_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_ohc=kbAk64VSYEwAX8KnqOa&_nc_ht=scontent.fpoz3-1.fna&oh=00_AfAEs2Ey6IdhllRHlzysqJJV8ViwwV1CBwnZ4JzDfAFprQ&oe=6401A409'
-				/>
-				<div className={styles.description}>
-					<h2 className={styles.name}>Uki</h2>
-					<p className={styles.nickname}>Tuki</p>
-					<div className={styles.following}>
-						<div className={styles.followers}>
-							<BsFileEarmarkCheckFill color='white'/>
-							<p className={styles.text}>Following (4)</p>
-						</div>
-						<div className={styles.followers}>
-							<BsFileEarmarkPersonFill color='white'/>
-							<p className={styles.text}>Followers (20)</p>
+		<div className={styles.section}>
+			<button onClick={() => navigate(-1)} className={styles.return}>
+				<BsFillArrowUpLeftSquareFill color='white' />
+				<p className={styles.back_txt}>Go back</p>
+			</button>
+			<div className={styles.content}>
+				<div className={styles.user}>
+					<img className={styles.photo} alt='a user' src={userData.image} />
+					<div className={styles.description}>
+						<h2 className={styles.name}>
+							{userData.firstName} {userData.lasttName}
+						</h2>
+						<p className={styles.nickname}>{userData.username}</p>
+						<div className={styles.following}>
+							<div className={styles.followers}>
+								<BsFileEarmarkCheckFill color='white' />
+								<p className={styles.text}>Following {userData.height}</p>
+							</div>
+							<div className={styles.followers}>
+								<BsFileEarmarkPersonFill color='white' />
+								<p className={styles.text}>Followers {userData.weight.toFixed(0)}</p>
+							</div>
 						</div>
 					</div>
 				</div>
+				<a href='github.com/'>
+					<Button dark text='View On Github' />
+				</a>
 			</div>
-			<a href='github.com/'>
-				<Button dark text='View On Github' />
-			</a>
 		</div>
 	);
 };
 
 export default UserDetails;
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+	console.log(params);
+
+	try {
+		const response = await axios.get(
+			`https://dummyjson.com/users/${params.userId}`
+		);
+		const data: UserType = response.data;
+		return data;
+	} catch {}
+};
